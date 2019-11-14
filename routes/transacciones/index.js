03a932a606adcebf624f7ea19dec4aadfc1963cc
Pablo Piñeiro <pmpv05@hotmail.com>
2019-11-14 16:30:58 -0300

    Update index.js

--- routes/transacciones/index.js
+++ routes/transacciones/index.js
@@ -18,7 +18,7 @@ module.exports = router => {
   );
 
   router.delete('/transacciones/:id', validators.uncreate, handlers.uncreate);
-  router.patch('/transacciones/:id', validators.update, handlers.update);
+  router.patch('/transacciones/:id', handlers.update);
 
   return router;
 };
